2578d2adf55ef1741f0ce7d0cca0db4bba83f7f1
Victor Mourão <pvictormourao@gmail.com>
2020-08-08 15:14:44 -0300

    UPDATE: Adding TransactionsRepository methods all, getBalance and create, adding CreateTransactionService method execute and get and post routes for transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Check the type of the transaction and if it is less than the total balance
+    if (type === 'outcome') {
+      if (this.transactionsRepository.getBalance().total - value < 0) {
+        throw new Error(
+          'Outcome transactions must be less than the total balance',
+        );
+      }
+    }
+    // Create the transaction in the TransactionsRepository
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
